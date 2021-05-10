ca96f3da8debfd6ef4fa757e011ed39334808b76
Cícero Junior <cicerojunior.cont@gmail.com>
2021-05-09 21:44:25 -0300

    test: conjunto de testes de validações

--- test/anagrama.spec.ts
+++ test/anagrama.spec.ts
@@ -11,4 +11,29 @@ describe("Anagrama", () => {
         expect(functionCreateWithError).toThrow("Palavras com tamanhos diferentes");
     });
 
+    it(`validarSeEhAnagrama deve lançar um erro 'Parametro 1 inválido, pois está vazio ou nulo'`, async () => {  
+        let functionCreateWithError = ()=>{Anagrama.validarSeEhAnagrama("","uepahcs")};    
+        expect(functionCreateWithError).toThrow('Parametro 1 inválido, pois está vazio ou nulo');
+    });
+
+    it(`validarSeEhAnagrama deve lançar um erro 'Parametro 1 inválido, pois está vazio ou nulo'`, async () => {  
+        let functionCreateWithError = ()=>{Anagrama.validarSeEhAnagrama(null,"uepahcs")};    
+        expect(functionCreateWithError).toThrow('Parametro 1 inválido, pois está vazio ou nulo');
+    });
+
+    it(`validarSeEhAnagrama deve lançar um erro 'Parametro 2 inválido, pois está vazio ou nulo'`, async () => {  
+        let functionCreateWithError = ()=>{Anagrama.validarSeEhAnagrama("chapeu","")};    
+        expect(functionCreateWithError).toThrow('Parametro 2 inválido, pois está vazio ou nulo');
+    });
+
+    it(`validarSeEhAnagrama deve lançar um erro 'Parametro 2 inválido, pois está vazio ou nulo'`, async () => {  
+        let functionCreateWithError = ()=>{Anagrama.validarSeEhAnagrama("chapeu", null)};    
+        expect(functionCreateWithError).toThrow('Parametro 2 inválido, pois está vazio ou nulo');
+    });
+
+    it(`validarSeEhAnagrama deve lançar um erro 'Não somos palhaços para validar anagramas de apenas 1 digito'`, async () => {  
+        let functionCreateWithError = ()=>{Anagrama.validarSeEhAnagrama("a", "a")};    
+        expect(functionCreateWithError).toThrow('Não somos palhaços para validar anagramas de apenas 1 digito');
+    });
+
 });
